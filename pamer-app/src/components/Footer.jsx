4d74481ddcc2f802321c098a64b8c164134b95c0
SebastianYauri <sebas123.sys@gmail.com>
2024-05-22 17:54:26 -0500

    footer-cambio de contenido

--- pamer-app/src/components/Footer.jsx
+++ pamer-app/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BsDribbble, BsFacebook, BsGithub, BsInstagram, BsTwitter } from "react-icons/bs";
+import { BsFacebook, BsInstagram, BsTwitter ,BsTiktok } from "react-icons/bs";
 
 const CustomFooter = () => {
   return (
@@ -8,21 +8,23 @@ const CustomFooter = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
           <div>
             <a href="/logo.svg" className="mb-4 flex items-center">
-              <img alt="Pamer Logo" src="/logo.svg" className="mr-3 h-8" />
+              <img alt="Pamer Logo" src="/logo-white.svg" className="mr-3 h-8" />
             </a>
           </div>
           <div>
-            <h2 className="mb-6 text-sm font-semibold uppercase">About</h2>
+            <h2 className="mb-6 text-sm font-semibold uppercase">Contacto</h2>
             <ul className="flex flex-col space-y-4 text-sm">
-              <li><a href="#" className="hover:underline">Flowbite</a></li>
-              <li><a href="#" className="hover:underline">Tailwind CSS</a></li>
+              <li><a href="#" className="hover:underline">Email: informes_academias@pamer.edu.pe</a></li>
+              <li><a href="#" className="hover:underline">Central telf: 01 748 3196</a></li>
             </ul>
           </div>
           <div>
-            <h2 className="mb-6 text-sm font-semibold uppercase">Follow us</h2>
+            <h2 className="mb-6 text-sm font-semibold uppercase">Redes Sociales</h2>
             <ul className="flex flex-col space-y-4 text-sm">
-              <li><a href="#" className="hover:underline">Github</a></li>
-              <li><a href="#" className="hover:underline">Discord</a></li>
+              <li><a href="#" className="hover:underline">Facebook</a></li>
+              <li><a href="#" className="hover:underline">Instagram</a></li>
+              <li><a href="#" className="hover:underline">Tiktok</a></li>
+              <li><a href="#" className="hover:underline">Youtube</a></li>
             </ul>
           </div>
           <div>
@@ -36,14 +38,13 @@ const CustomFooter = () => {
         <hr className="my-6 border-gray-200" />
         <div className="flex justify-center items-center mt-4">
           <div className="text-sm">
-            © 2022 <span className="font-semibold">Pamer</span>
+            © 2024 Corporación Educativa <span className="font-semibold">Pamer</span>
           </div>
           <div className="ml-6 flex space-x-6">
             <a href="#" className="text-gray-500 hover:text-white"><BsFacebook /></a>
             <a href="#" className="text-gray-500 hover:text-white"><BsInstagram /></a>
             <a href="#" className="text-gray-500 hover:text-white"><BsTwitter /></a>
-            <a href="#" className="text-gray-500 hover:text-white"><BsGithub /></a>
-            <a href="#" className="text-gray-500 hover:text-white"><BsDribbble /></a>
+            <a href="#" className="text-gray-500 hover:text-white"><BsTiktok /></a>
           </div>
         </div>
       </div>
