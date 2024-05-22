53f39ffad205f62e560c8e06807642df97978fd8
SebastianYauri <sebas123.sys@gmail.com>
2024-05-22 18:00:08 -0500

    Footer-correcion de columnas

--- pamer-app/src/components/Footer.jsx
+++ pamer-app/src/components/Footer.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { BsFacebook, BsInstagram, BsTwitter ,BsTiktok } from "react-icons/bs";
+import { BsFacebook, BsInstagram, BsTwitter, BsTiktok } from "react-icons/bs";
 
 const CustomFooter = () => {
   return (
     <footer className="fixed bottom-0 w-full bg-blue-900 text-white p-4">
       <div className="container mx-auto py-6">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
           <div>
             <a href="/logo.svg" className="mb-4 flex items-center">
               <img alt="Pamer Logo" src="/logo-white.svg" className="mr-3 h-8" />
@@ -35,16 +35,16 @@ const CustomFooter = () => {
             </ul>
           </div>
         </div>
-        <hr className="my-6 border-gray-200" />
-        <div className="flex justify-center items-center mt-4">
-          <div className="text-sm">
+        <hr className="my-6 border-gray-200 border-opacity-30" />
+        <div className="flex flex-col md:flex-row justify-between items-center mt-4">
+          <div className="text-sm mb-4 md:mb-0">
             © 2024 Corporación Educativa <span className="font-semibold">Pamer</span>
           </div>
-          <div className="ml-6 flex space-x-6">
-            <a href="#" className="text-gray-500 hover:text-white"><BsFacebook /></a>
-            <a href="#" className="text-gray-500 hover:text-white"><BsInstagram /></a>
-            <a href="#" className="text-gray-500 hover:text-white"><BsTwitter /></a>
-            <a href="#" className="text-gray-500 hover:text-white"><BsTiktok /></a>
+          <div className="flex space-x-6">
+            <a href="#" className="text-gray-500 hover:text-white"><BsFacebook className="w-5 h-5" /></a>
+            <a href="#" className="text-gray-500 hover:text-white"><BsInstagram className="w-5 h-5" /></a>
+            <a href="#" className="text-gray-500 hover:text-white"><BsTwitter className="w-5 h-5" /></a>
+            <a href="#" className="text-gray-500 hover:text-white"><BsTiktok className="w-5 h-5" /></a>
           </div>
         </div>
       </div>
