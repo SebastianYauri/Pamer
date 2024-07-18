b4a13923febcc64cfe6fd36c6394632d43ea1933
Truegii <erickgianpierre@gmail.com>
2024-07-18 13:55:47 -0500

    Actualizar nota

--- pamer-app/src/pages/SectionDetail.jsx
+++ pamer-app/src/pages/SectionDetail.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { motion } from 'framer-motion';
 import { FaMapMarkerAlt, FaClock, FaInfoCircle, FaGraduationCap } from 'react-icons/fa';
-import { BASE_URL } from '../config/apiConfig'; 
+import { BASE_URL } from '../config/apiConfig';
 
 const SectionDetail = () => {
   const { id } = useParams(); // Obtener el id de la sección desde los parámetros de la ruta
@@ -58,39 +58,47 @@ const SectionDetail = () => {
 
   const handleSubmit = async () => {
     const processedGrades = {
-        n1: grades.note1 ? parseFloat(grades.note1) : 0,
-        n2: grades.note2 ? parseFloat(grades.note2) : 0,
-        n3: grades.note3 ? parseFloat(grades.note3) : 0,
-        n4: grades.note4 ? parseFloat(grades.note4) : 0,
-        n5: grades.note5 ? parseFloat(grades.note5) : 0,
+      n1: grades.note1 ? parseFloat(grades.note1) : 0,
+      n2: grades.note2 ? parseFloat(grades.note2) : 0,
+      n3: grades.note3 ? parseFloat(grades.note3) : 0,
+      n4: grades.note4 ? parseFloat(grades.note4) : 0,
+      n5: grades.note5 ? parseFloat(grades.note5) : 0,
     };
 
     try {
-        // Obtener detalles completos de Alumno antes de enviar la solicitud
-        const responseAlumno = await axios.get(`${BASE_URL}:8080/alumnos/${selectedStudent}`);
-        const alumno = responseAlumno.data;
+      // Obtener detalles completos de Alumno antes de enviar la solicitud
+      const responseAlumno = await axios.get(`${BASE_URL}:8080/alumnos/${selectedStudent}`);
+      const alumno = responseAlumno.data;
 
-        const ciclo = { id: id }; // No necesita obtenerlo si ya lo tienes en detalle en el frontend
+      const ciclo = { id: id }; // No necesita obtenerlo si ya lo tienes en detalle en el frontend
 
-        const notaData = {
-            alumno: alumno,
-            ciclo: ciclo,
-            ...processedGrades,
-        };
+      const notaData = {
+        alumno: alumno,
+        ciclo: ciclo,
+        ...processedGrades,
+      };
 
-        const response = await axios.post(`${BASE_URL}:8090/notas/guardar`, notaData);
-        console.log(response.data);
-        alert("Notas registradas con éxito");
-        // Volver a cargar los registros de notas
-        
+      const response = await axios.post(`${BASE_URL}:8090/notas/guardar`, notaData);
+      console.log(response.data);
+      alert("Notas registradas con éxito");
+      // Volver a cargar los registros de notas
+      //fetchGradeRecords();
+      window.location.reload();
     } catch (error) {
-        console.error("Error al registrar las notas:", error.response ? error.response.data : error.message);
-        alert("Error al registrar las notas");
+      console.error("Error al registrar las notas:", error.response ? error.response.data : error.message);
+      alert("Error al registrar las notas");
     }
   };
 
-  const handlePrint = () => {
-    window.print(); // Función para imprimir la página
+  const handleUpdate = (record) => {
+    setSelectedStudent(record.alumno.id);
+    setGrades({
+      note1: record.n1,
+      note2: record.n2,
+      note3: record.n3,
+      note4: record.n4,
+      note5: record.n5,
+    });
   };
 
   return (
@@ -202,14 +210,14 @@ const SectionDetail = () => {
           </select>
         </div>
         <div className="grid grid-cols-5 gap-4 mb-4">
-          {['note1', 'note2', 'note3', 'note4', 'note5'].map((note, index) => (
-            <div key={index}>
-              <label htmlFor={note} className="block mb-2">{`Nota ${index + 1}`}</label>
+          {[1, 2, 3, 4, 5].map(num => (
+            <div key={num}>
+              <label htmlFor={`note${num}`} className="block mb-2">Nota {num}:</label>
               <input
                 type="number"
-                id={note}
-                name={note}
-                value={grades[note]}
+                id={`note${num}`}
+                name={`note${num}`}
+                value={grades[`note${num}`]}
                 onChange={handleGradeChange}
                 className="w-full p-2 border border-gray-300 rounded"
               />
@@ -218,12 +226,12 @@ const SectionDetail = () => {
         </div>
         <button
           onClick={handleSubmit}
-          className="px-4 py-2 bg-blue-500 text-white rounded"
+          className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
         >
-          Registrar Notas
+          Guardar Notas
         </button>
       </div>
-
+      {/* Tabla de registros de notas */}
       <div className="mt-8">
         <motion.h2
           className="text-3xl font-bold mb-4"
@@ -231,34 +239,38 @@ const SectionDetail = () => {
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.5 }}
         >
-          Alumnos y Notas
+          Registros de Notas
         </motion.h2>
-        {/* Tabla de alumnos y notas */}
         <div className="overflow-x-auto">
-          <table className="min-w-full bg-white border">
+          <table className="min-w-full bg-white border border-gray-300">
             <thead>
               <tr>
-                <th className="px-4 py-2 border">ID</th>
-                <th className="px-4 py-2 border">Nombre</th>
-                <th className="px-4 py-2 border">Apellido</th>
-                <th className="px-4 py-2 border">Nota 1</th>
-                <th className="px-4 py-2 border">Nota 2</th>
-                <th className="px-4 py-2 border">Nota 3</th>
-                <th className="px-4 py-2 border">Nota 4</th>
-                <th className="px-4 py-2 border">Nota 5</th>
+                <th className="py-2 px-4 border-b">Alumno</th>
+                <th className="py-2 px-4 border-b">Nota 1</th>
+                <th className="py-2 px-4 border-b">Nota 2</th>
+                <th className="py-2 px-4 border-b">Nota 3</th>
+                <th className="py-2 px-4 border-b">Nota 4</th>
+                <th className="py-2 px-4 border-b">Nota 5</th>
+                <th className="py-2 px-4 border-b">Acción</th>
               </tr>
             </thead>
             <tbody>
-              {gradeRecords.map((record) => (
-                <tr key={record.alumno.id}>
-                  <td className="px-4 py-2 border">{record.alumno.id}</td>
-                  <td className="px-4 py-2 border">{record.alumno.nombre}</td>
-                  <td className="px-4 py-2 border">{record.alumno.apellido}</td>
-                  <td className="px-4 py-2 border">{record.n1}</td>
-                  <td className="px-4 py-2 border">{record.n2}</td>
-                  <td className="px-4 py-2 border">{record.n3}</td>
-                  <td className="px-4 py-2 border">{record.n4}</td>
-                  <td className="px-4 py-2 border">{record.n5}</td>
+              {gradeRecords.map(record => (
+                <tr key={record.id}>
+                  <td className="py-2 px-4 border-b">{record.alumno.nombre} {record.alumno.apellido}</td>
+                  <td className="py-2 px-4 border-b">{record.n1}</td>
+                  <td className="py-2 px-4 border-b">{record.n2}</td>
+                  <td className="py-2 px-4 border-b">{record.n3}</td>
+                  <td className="py-2 px-4 border-b">{record.n4}</td>
+                  <td className="py-2 px-4 border-b">{record.n5}</td>
+                  <td className="py-2 px-4 border-b">
+                    <button
+                      onClick={() => handleUpdate(record)}
+                      className="bg-green-500 text-white p-2 rounded hover:bg-green-600"
+                    >
+                      Actualizar
+                    </button>
+                  </td>
                 </tr>
               ))}
             </tbody>
