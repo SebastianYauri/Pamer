74bf985d7fa38d6cf4b7f40d3abd52d99abf89df
SebastianYauri <sebas123.sys@gmail.com>
2024-07-19 04:30:47 -0500

    Update:botonesPDF

--- pamer-app/src/pages/StudentView.jsx
+++ pamer-app/src/pages/StudentView.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -6,13 +6,15 @@ import { FaBook, FaClock, FaChalkboard, FaUserGraduate, FaRegStar } from 'react-
 import { motion } from 'framer-motion';
 import axios from 'axios'; // Importa axios para hacer llamadas a la API
 import { BASE_URL } from '../config/apiConfig';
+import { useReactToPrint } from 'react-to-print';
 
 const StudentView = () => {
   const [isOpen, setIsOpen] = useState(true); // Estado para controlar la visibilidad del sidebar
   const [gradesData, setGradesData] = useState([]); // Estado para almacenar las notas del alumno
   const [alumno, setAlumno] = useState(null); // Estado para almacenar los datos del alumno
   const [matriculasData, setMatriculasData] = useState([]); // Estado para almacenar las matriculas del alumno
-
+  const componentRef = useRef();
+  const scheduleRef = useRef();
   // Función para alternar la visibilidad del sidebar
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
@@ -89,6 +91,14 @@ const StudentView = () => {
     { time: '13:00 - 14:00', Lunes: '', Martes: '', Miércoles: '', Jueves: '', Viernes: '' },
   ];
 
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
+  const handlePrintSchedule = useReactToPrint({
+    content: () => scheduleRef.current,
+  });
+  
+
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-4 flex items-center space-x-2">
@@ -126,7 +136,7 @@ const StudentView = () => {
       </div>
 
       {/* Sección de Horarios */}
-      <div className="mb-8">
+      <div ref={scheduleRef} className="mb-8">
         <h2 className="text-xl font-semibold mb-4 flex items-center space-x-2">
           <FaClock className="text-blue-500 animate-bounce" />
           <motion.span
@@ -162,54 +172,79 @@ const StudentView = () => {
         </div>
       </div>
 
-      {/* Sección de Notas */}
-      <div>
-        <h2 className="text-xl font-semibold mb-4 flex items-center space-x-2">
-          <FaBook className="text-blue-500 animate-bounce" />
-          <motion.span
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 1 }}
-          >
-            Notas
-          </motion.span>
-        </h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {gradesData.map((grade, index) => (
-            <div key={index} className="bg-white shadow-md rounded-lg p-6 flex flex-col space-y-4">
-              <div className="flex items-center space-x-4">
-                <FaRegStar className="text-4xl text-blue-500" />
-                <div>
-                  <h3 className="text-lg font-semibold">Ciclo: {grade.ciclo.ciclo}</h3>
-                  <p className="text-sm text-gray-500">Alumno: {grade.alumno.nombre}</p>
-                </div>
-              </div>
-              <div className="grid grid-cols-2 gap-4 text-center">
-                <div className="bg-gray-100 p-2 rounded-lg">
-                  <p className="font-bold">N1</p>
-                  <p>{grade.n1}</p>
-                </div>
-                <div className="bg-gray-100 p-2 rounded-lg">
-                  <p className="font-bold">N2</p>
-                  <p>{grade.n2}</p>
-                </div>
-                <div className="bg-gray-100 p-2 rounded-lg">
-                  <p className="font-bold">N3</p>
-                  <p>{grade.n3}</p>
-                </div>
-                <div className="bg-gray-100 p-2 rounded-lg">
-                  <p className="font-bold">N4</p>
-                  <p>{grade.n4}</p>
-                </div>
-                <div className="bg-gray-100 p-2 rounded-lg">
-                  <p className="font-bold">N5</p>
-                  <p>{grade.n5}</p>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
+            {/* Botón para imprimir la sección de horarios */}
+            <div className="mt-4 mb-8">
+        <button
+          onClick={handlePrintSchedule}
+          className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow-md hover:bg-blue-600"
+        >
+          Imprimir Horarios
+        </button>
       </div>
+
+      {/* Sección de Notas */}
+<div ref={componentRef}>
+  <h2 className="text-xl font-semibold mb-4 flex items-center space-x-2">
+    <FaBook className="text-blue-500 animate-bounce" />
+    <motion.span
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
+      Reporte de Notas
+    </motion.span>
+  </h2>
+  <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+    {gradesData.map((grade, index) => (
+      <div key={index} className="bg-white shadow-md rounded-lg p-6 flex flex-col space-y-4">
+        <div className="flex items-center space-x-4">
+          <FaRegStar className="text-4xl text-blue-500" />
+          <div>
+            <h3 className="text-lg font-semibold text-2xl">Universidad: {grade.ciclo.universidad}</h3>
+            <p className="text-sm text-gray-500 text-xl">Ciclo: {grade.ciclo.ciclo}</p>
+            <p className="text-sm text-gray-500 text-xl">Alumno: {grade.alumno.nombre} {grade.alumno.apellido}</p>
+          </div>
+        </div>
+        <div className="grid grid-cols-5 gap-4 text-center"> {/* Cambiado a 5 columnas */}
+          <div className="bg-gray-100 p-2 rounded-lg">
+            <p className="font-bold">Nota 1</p>
+            <p>{grade.n1}</p>
+          </div>
+          <div className="bg-gray-100 p-2 rounded-lg">
+            <p className="font-bold">Nota 2</p>
+            <p>{grade.n2}</p>
+          </div>
+          <div className="bg-gray-100 p-2 rounded-lg">
+            <p className="font-bold">Nota 3</p>
+            <p>{grade.n3}</p>
+          </div>
+          <div className="bg-gray-100 p-2 rounded-lg">
+            <p className="font-bold">Nota 4</p>
+            <p>{grade.n4}</p>
+          </div>
+          <div className="bg-gray-100 p-2 rounded-lg">
+            <p className="font-bold">Nota 5</p>
+            <p>{grade.n5}</p>
+          </div>
+        </div>
+          <div className="mt-8">
+    <        img src="/boleta.jpg" alt="Notas Imagen" className="w-full h-auto rounded-lg shadow-md" />
+          </div>
+      </div>
+      
+    ))}
+  </div>
+</div>
+
+<div className="mt-4 mb-8">
+  <button
+    onClick={handlePrint}
+    className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow-md hover:bg-blue-600"
+  >
+    Exportar Notas en PDF
+  </button>
+</div>
+
 
       {/* Sección de Matrículas */}
       <div>
