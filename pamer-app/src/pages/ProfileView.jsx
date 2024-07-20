5b98386041a7729c9ca4b39c629a44ba5eb899bd
SebastianYauri <sebas123.sys@gmail.com>
2024-07-19 21:16:52 -0500

    Fix-ListarDatosProfile

--- pamer-app/src/pages/ProfileView.jsx
+++ pamer-app/src/pages/ProfileView.jsx
@@ -5,7 +5,8 @@ import { BASE_URL } from '../config/apiConfig';
 
 const ProfileView = () => {
   const [alumno, setAlumno] = useState(null); // Estado para almacenar los datos del alumno
-  
+  const [matriculasData, setMatriculasData] = useState([]); // Estado para almacenar las matriculas del alumno
+
   // Hook para obtener el objeto Alumno desde localStorage
   useEffect(() => {
     const storedAlumno = localStorage.getItem("alumno");
@@ -14,17 +15,41 @@ const ProfileView = () => {
     }
   }, []);
 
+  // Hook para obtener las matriculas del alumno
+  useEffect(() => {
+    const fetchMatriculas = async () => {
+      if (alumno) {
+        try {
+          const response = await axios.get(`${BASE_URL}:8080/matricula/listarPorAlumno`, {
+            params: { idAlumno: alumno.id },
+          });
+          setMatriculasData(response.data);
+        } catch (error) {
+          console.error("Error fetching matriculas data:", error);
+        }
+      }
+    };
+
+    fetchMatriculas();
+  }, [alumno]);
+
   // Datos del perfil del alumno
   const fetchAlumnoData = async () => {
-    try {
-      const response = await axios.get(`${BASE_URL}:8080/alumnos/perfil`, {
-        params: { idAlumno: alumno.id },
-      });
-      setAlumno(response.data);
-    } catch (error) {
-      console.error('Error al obtener los datos del estudiante:', error);
+    if (alumno?.id) { // Asegúrate de que alumno.id esté definido
+      try {
+        console.log("Fetching data for idAlumno:", alumno.id); // Agrega este log
+        const response = await axios.get(`${BASE_URL}:8080/alumnos/perfil`, {
+          params: { idAlumno: alumno.id },
+        });
+        setAlumno(response.data);
+      } catch (error) {
+        console.error('Error al obtener los datos del estudiante:', error.response?.data || error.message); // Agrega error.response?.data para ver el mensaje de error
+      }
+    } else {
+      console.error('ID del alumno no está definido.');
     }
   };
+  
 
   useEffect(() => {
     if (alumno) {
@@ -32,68 +57,76 @@ const ProfileView = () => {
     }
   }, [alumno]);
 
+  // Obtiene el primer ciclo de la lista de matriculas
+  const ciclo = matriculasData.length > 0 ? matriculasData[0].ciclo : null;
+
+  // Extrae propiedades relevantes del objeto ciclo si es necesario
+  const cicloNombre = ciclo ? ciclo.ciclo : 'No disponible';
+  const cicloModalidad = ciclo ? ciclo.modalidad : 'No disponible';
+  const cicloUniversidad = ciclo ? ciclo.universidad : 'No disponible';
+
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="bg-white shadow-md rounded-lg p-6">
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center mb-6">
           <img
             src="/profile.webp"
             alt="Profile"
             className="w-32 h-32 rounded-full mb-4"
           />
           <h2 className="text-2xl font-bold mb-2">{alumno?.nombre} {alumno?.apellido}</h2>
-          <p className="text-gray-600 mb-4">{alumno?.universidad}</p>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div className="flex items-center">
-            <FaUser className="text-blue-500 mr-2" />
+            <FaUser className="text-blue-500 mr-4 text-xl" />
             <div>
-              <h3 className="text-xl font-bold mb-2">Nombre</h3>
+              <h3 className="text-xl font-bold mb-1">Nombre</h3>
               <p className="text-gray-800">{alumno?.nombre}</p>
             </div>
           </div>
           <div className="flex items-center">
-            <FaUser className="text-blue-500 mr-2" />
+            <FaUser className="text-blue-500 mr-4 text-xl" />
             <div>
-              <h3 className="text-xl font-bold mb-2">Apellido</h3>
+              <h3 className="text-xl font-bold mb-1">Apellido</h3>
               <p className="text-gray-800">{alumno?.apellido}</p>
             </div>
           </div>
           <div className="flex items-center">
-            <FaUniversity className="text-blue-500 mr-2" />
+            <FaUniversity className="text-blue-500 mr-4 text-xl" />
             <div>
-              <h3 className="text-xl font-bold mb-2">Universidad</h3>
-              <p className="text-gray-800">{alumno?.universidad?.nombre || 'No disponible'}</p> {/* Ajusta según la estructura de tu API */}
+              <h3 className="text-xl font-bold mb-1">Universidad</h3>
+              <p className="text-gray-800">{cicloUniversidad}</p>
             </div>
           </div>
           <div className="flex items-center">
-            <FaIdCard className="text-blue-500 mr-2" />
+            <FaIdCard className="text-blue-500 mr-4 text-xl" />
             <div>
-              <h3 className="text-xl font-bold mb-2">DNI</h3>
+              <h3 className="text-xl font-bold mb-1">DNI</h3>
               <p className="text-gray-800">{alumno?.dni}</p>
             </div>
           </div>
           <div className="flex items-center">
-            <FaEnvelope className="text-blue-500 mr-2" />
+            <FaEnvelope className="text-blue-500 mr-4 text-xl" />
             <div>
-              <h3 className="text-xl font-bold mb-2">Correo</h3>
+              <h3 className="text-xl font-bold mb-1">Correo</h3>
               <p className="text-gray-800">{alumno?.correo}</p>
             </div>
           </div>
           <div className="flex items-center">
-            <FaRegCalendarAlt className="text-blue-500 mr-2" />
+            <FaRegCalendarAlt className="text-blue-500 mr-4 text-xl" />
             <div>
-              <h3 className="text-xl font-bold mb-2">Ciclo</h3>
-              <p className="text-gray-800">{alumno?.ciclo?.nombre || 'No disponible'}</p> {/* Ajusta según la estructura de tu API */}
+              <h3 className="text-xl font-bold mb-1">Ciclo</h3>
+              <p className="text-gray-800">{cicloNombre}</p>
+              <p className="text-gray-800">{cicloModalidad}</p>
             </div>
           </div>
         </div>
       </div>
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center mt-8">
         <img
           src="/profile2.jpg"
           alt="Profile"
-          className="w-90 h-80 mb-4"
+          className="w-90 h-80"
         />
       </div>
     </div>
